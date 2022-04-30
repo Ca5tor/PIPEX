6793c2c6f623a25bf3c53bb0d904c1cc26ff9218
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2022-04-30 17:14:25 +0300

    Header pipex

---
+++ pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltacos <ltacos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 21:53:13 by ltacos            #+#    #+#             */
+/*   Updated: 2022/04/29 20:48:58 by ltacos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include "Libft/libft.h"
+
+void	error_msg(char *msg);
+void	free_mtx_ch(char **mtx);
+
+#endif
